b0fe69c81ec973161abb9eea38283351d0fb506f
jguillen70 <jgguillen24@students.desu.edu>
2025-03-14 17:34:26 -0400

    Complete

--- src/main/java/SkillBuilder4.java
+++ src/main/java/SkillBuilder4.java
@@ -8,36 +8,43 @@
  */
 
 public class SkillBuilder4 {
+    public static String findTYpattern(String s) {
+        int state = 0;
+        int i = 0;
+        StringBuilder substring = new StringBuilder();
 
-    // replace this line with your constants
-        public static String containsTtoY(String s) {
-            int state = 0;
-            int i = 0;
-
-            while (i < s.length()) {
-                char c = s.charAt(i);
-                if (state == 0 && (c == 't' || c == 'T')) {
-                    state = 1;
-                } else if (state == 1) {
-                    if (c == 'y' || c == 'Y') {
-                        return "Substring found";
-                    } else {
-                        state = 2;
-                    }
-                } else if (state == 2 && (c == 'y' || c == 'Y')) {
-                    return "Substring found";
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            if (state == 0 && (c == 't' || c == 'T')) {
+                state = 1;
+                substring.append(c);
+            } else if (state == 1) {
+                substring.append(c);
+                if (c == 'y' || c == 'Y') {
+                    return "Substring found: " + substring;
+                } else {
+                    state = 2;
                 }
-                i++;
+            } else if (state == 2) {
+                substring.append(c);
+                if (c == 'y' || c == 'Y') {
+                    return "Substring found: " + substring;
+                }
             }
-            return "Substring not found";
+            i++;
         }
-
-        public static void main(String[] args) {
-            System.out.println(containsTtoY("abcTxyz")); // Substring found
-            System.out.println(containsTtoY("random texty")); // Substring found
-            System.out.println(containsTtoY("xyz")); // Substring not found
-            System.out.println(containsTtoY("tHelloY")); // Substring found
-            System.out.println(containsTtoY("ty")); // Substring found
-        }
+        return "Substring not found";
     }
+
+    public static void main(String[] args) {
+        System.out.println(findTYpattern("abcTxyz")); // Substring found: Txy
+        System.out.println(findTYpattern("random texty")); // Substring found: texty
+        System.out.println(findTYpattern("xyz")); // Substring not found
+        System.out.println(findTYpattern("tHelloY")); // Substring found: tHelloY
+        System.out.println(findTYpattern("ty")); // Substring found: ty
+    }
+}
+
+
+
 
